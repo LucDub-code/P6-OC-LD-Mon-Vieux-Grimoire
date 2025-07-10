cfc565539892f8f2987bce1c53780a66d5b947ee
LucDub-code <contact@lucasdubeau.dev>
2025-07-10 16:06:24 +0200

    Connexion à MongoDB Atlas avec Mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect('mongodb+srv://lucdubcom:dLA3aNy78aIBj7Sk@monvieuxgrimoire.lv15ti6.mongodb.net/?retryWrites=true&w=majority&appName=MonVieuxGrimoire',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB Atlas réussie !'))
+  .catch(() => console.log('Connexion à MongoDB Atlas échouée !'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); 
+  next();
+});
+
+app.get('/test', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
 
 module.exports = app;

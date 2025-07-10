a91a173675a3b46ecef1f080e1a0d02f62bb32e1
LucDub-code <lucdubcom@gmail.com>
2025-07-10 17:40:40 +0200

    Implémentation du router, du controller et du schéma book, test getAllBook ok

---
+++ backend/routes/book.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const bookCtrl = require('../controllers/book');
+
+router.get('/', bookCtrl.getAllBooks);
+
+module.exports = router;

55170265012c8977be0e31bd876f57beb60b2f42
LucDub-code <contact@lucasdubeau.dev>
2025-07-11 19:02:33 +0200

    Mise en place du système d'authentification

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const bookRoutes = require('./routes/book');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -25,5 +26,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/books', bookRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const bookCtrl = require('../controllers/book');
+const bookController = require('../controllers/book');
 
-router.get('/', bookCtrl.getAllBooks);
+router.get('/', bookController.getAllBooks);
 
 module.exports = router;

0d498e7bb8c91a8fd10c6c0f18d78291e1234a9d
LucDub-code <lucdubcom@gmail.com>
2025-07-13 16:20:40 +0200

    Ajout de la fonctionnalité d'ajout de livre avec auth et multer

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const bookController = require('../controllers/book');
+const auth = require('../middlewares/auth');
+const multer = require('../middlewares/multer-config');
 
 router.get('/', bookController.getAllBooks);
+router.post('/', auth, multer, bookController.createBook);
 
 module.exports = router;

d9e16b487e7e639b3d7285ae998383eee1da9788
LucDub-code <lucdubcom@gmail.com>
2025-07-15 19:44:48 +0200

    Ajout du middleware sharp avec le package Sharp pour l'optimisation des images

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
-const bookRoutes = require('./routes/book');
-const userRoutes = require('./routes/user');
+const bookRoutes = require("./routes/book");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
-console.log(process.env.MONGODB_URI);
-
-mongoose.connect(process.env.MONGODB_URI,
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB Atlas réussie !'))
-  .catch(() => console.log('Connexion à MongoDB Atlas échouée !'));
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB Atlas réussie !"))
+  .catch(() => console.log("Connexion à MongoDB Atlas échouée !"));
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); 
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); 
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); 
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
-app.use('/api/books', bookRoutes);
-app.use('/api/auth', userRoutes);
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/api/books", bookRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const bookController = require("../controllers/book");
 const auth = require("../middlewares/auth");
 const multer = require("../middlewares/multer-config");
+const sharp = require("../middlewares/sharp");
 
-router.post("/", auth, multer, bookController.createBook);
+router.post("/", auth, multer, sharp, bookController.createBook);
 router.post("/:id/rating", auth, bookController.addRating);
 router.get("/", bookController.getAllBooks);
 router.get("/bestrating", bookController.getBestRating);
 router.get("/:id", bookController.getOneBook);
-router.put("/:id", auth, multer, bookController.updateBook);
+router.put("/:id", auth, multer, sharp, bookController.updateBook);
 router.delete("/:id", auth, bookController.deleteBook);
 
 module.exports = router;
